c62c3369af3ee65322ea15ad18dca47f525f5cb7
Heng Zou <hzou@GSQUARED.local>
2017-10-13 11:06:01 +1100

    Start SSR

---
+++ client/src/js/routes_config.js
@@ -0,0 +1,37 @@
+import AppRoot from './containers/app';
+import Home from './components/home';
+import Category from './components/category';
+import Product from './components/product';
+import singleCategory from './components/singleCategory';
+import NotFound from './components/notfound';
+
+export const routes = [
+    {
+        component: AppRoot,
+        routes: [
+            {
+                path: '/',
+                exact: true,
+                component: Home
+            },
+            {
+                path: '/category',
+                exact: true,
+                component: Category
+            },
+            {
+                path: '/product',
+                exact: true,
+                component: Product
+            },
+            {
+                path: '/category/:id',
+                component: singleCategory
+            },
+            {
+                path: "*",
+                component: NotFound
+            }
+        ]
+    }
+]
