b33354a7c92088f6e53f44fa13f7e3da2b60d428
Heng Zou <hzou@GSQUARED.local>
2017-10-17 17:28:47 +1100

    Footer

--- client/src/js/components/sections/header.js
+++ client/src/js/components/sections/header.js
@@ -1,22 +1,31 @@
 import React from "react";
-import {link} from "react-router";
+import { Link } from "react-router-dom";
 import headerLogo from "../../../img/logo.png";
 
-export const Header = (props) => {
-    return (
-        <header className="site-heaader home-header">
-            <div className="site-inner">
-                <div className="header-logo">
-                    <img src={ headerLogo } alt="OzParty" />
+export default class Header extends React.Component {
+    constructor(){
+        super();
+    }
+
+    render (){
+
+        return (
+            <header className={ "site-header "+ (this.props.ifHome ? "home-header" : "") }>
+                <div className="site-inner">
+                    <div className="site-header-inner">
+                        <div className="header-logo">
+                            <Link to="/"><img src={ headerLogo } alt="OzParty" /></Link>
+                        </div>
+                        <div className="header-nav">
+                            <ul>
+                                <li><a href="#" className="header-enquiry-btn">Enquire Now</a></li>
+                                <li><a href="#" id="nav-btn" 
+                                onClick = { () => this.props.changeMenu() } ><i className="fa fa-bars" aria-hidden="true"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                <div className="header-nav">
-                    <ul>
-                        <li><a href="#" className="header-enquiry-btn">Enquire Now</a></li>
-                        <li><a href="#" id="nav-btn" 
-                        onClick = { () => props.changeMenu() } ><i className="fa fa-bars" aria-hidden="true"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-        </header>
-    );
-};
+            </header>
+        );
+    }
+}
--- client/src/js/containers/app.js
+++ client/src/js/containers/app.js
@@ -3,26 +3,36 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { renderRoutes } from 'react-router-config';
-import { Header } from "../components/sections/header";
+import  Header  from "../components/sections/header";
 import { Navigation } from "../components/sections/navigation";
 import Routes from "../routes";
 import { changeState } from "../actions/menuAction";
+import { Footer } from "../components/sections/footer";
 
 class App extends React.Component {
+
+    ifHome(){
+        let currentPath = this.props.location.pathname;
+        return (currentPath == "/") ? true:  ((currentPath == "/home") ? true : false);
+    }
+
+    componentDidMount(){
+        
+    }
 
     render() {
         return (
             <div className="container">
-                <div className="page-container">
-                    <Header changeMenu={ () => this.props.changeMenuState( this.props.menu.showMenu) }/>
-                    { this.props.children } 
-                </div>
+                <Header changeMenu={ () => this.props.changeMenuState( this.props.menu.showMenu) } ifHome = { this.ifHome() }/>
+
                 <div className="site-menu">
                      <Navigation menuState={ this.props.menu.showMenu } /> 
                 </div>
-                <div className="page-content">
+                <div className="page-main">
                     { renderRoutes( this.props.route.routes )}
-                </div> 
+                </div>
+
+                <Footer/>
             </div>
         );
     }
