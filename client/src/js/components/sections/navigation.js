d1846f3d238d0f8a095618f68a5ca41e45315505
Heng Zou <hzou@GSQUARED.local>
2017-10-18 12:03:40 +1100

    Slide menu done

--- client/src/js/components/sections/navigation.js
+++ client/src/js/components/sections/navigation.js
@@ -21,14 +21,34 @@ class Navigation extends React.Component{
     
         return (
             <nav id="site-navigation" className={`site-navigation ${showMenu}` }>
-                <div className="site-nav-header">
-                    <a href="#" id="site-nav-close" onClick={ (e) => {this.changeMenuState(e)} }>X</a>
+                <div className="site-nav-inner">
+                    <div className="site-nav-header">
+                        <a href="#" id="site-nav-close" onClick={ (e) => {this.changeMenuState(e)} }>X</a>
+                    </div>
+                    <div className="site-nav-middle">
+                        <ul>
+                            <li><NavLink to="/home" activeClassName="active">Home</NavLink></li>
+                            <li><NavLink to="/category" activeClassName="active">Category</NavLink></li>
+                            <li><NavLink to="/product" activeClassName="active">Product</NavLink></li>
+                        </ul>
+                    </div>
+                    <div className="site-nav-bottom">
+                        <div className="site-nav-info">
+                            <p><span>Call Us</span>: <a href="tel:1800697278">1800 697 278</a></p>
+                            <p><span>Email</span>: <a href="mailto:info@ozpartyevents.com.au">info@ozpartyevents.com.au</a></p>
+                            <p><span>Sydney office</span>: 124 George St, Sydney</p>
+                        </div>
+                        <div className="site-nav-social">
+                            <ul>
+                                <li><a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a></li>
+                                <li><a href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                <li><a href="#"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
+                                <li><a href="#"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                                <li><a href="#"><i className="fa fa-snapchat-ghost" aria-hidden="true"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                <ul>
-                    <li><NavLink to="/home" activeClassName="active">Home</NavLink></li>
-                    <li><NavLink to="/category" activeClassName="active">Category</NavLink></li>
-                    <li><NavLink to="/product" activeClassName="active">Product</NavLink></li>
-                 </ul>
             </nav>
         );
     }
