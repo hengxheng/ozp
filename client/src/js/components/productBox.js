ed0078c00ab9a428032513db1e0957e1d1cba9a4
Heng Zou <hzou@GSQUARED.local>
2017-10-23 10:47:03 +1100

    Add to fav function undone

--- client/src/js/components/productBox.js
+++ client/src/js/components/productBox.js
@@ -1,38 +1,75 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { addToFav } from "../helper";
+import { addToFav, removeFromFav, checkFavExits } from "../helper";
 
-export const ProductBox = (props) => {
-    let imgStyle = {
-        background: '#c6c6c6'
-    };
-    if(props.product.image != null){
-        imgStyle = {
-            background: `url(${props.product.image}) no-repeat center center`,
-            backgroundColor: '#c6c6c6',
-            backgroundSize: 'cover'
+export class ProductBox extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            fav_class : "add-to-fav",
+            fav_added : false
+        }
+    }
+    
+    componentDidMount(){
+        if(checkFavExits(this.props.product.id)){
+            this.setState({
+                fav_class : "add-to-fav added-to-fav",
+                fav_added : true
+            });
         }
     }
 
-    return (
-        <div id={ props.product.id }className="product-box">
-            <Link className="product-box-link" to={ `/product/${props.product.id}` }>
-                <div className="product-img-box" style={ imgStyle }>
-                    <div className="product-name">{ props.product.name }</div>
-                    <div className="product-desc">
-                        <div className="p-name">
-                            { props.product.name }
-                        </div>
-                        <div className="p-desc">
-                            { props.product.desc }
+    
+    updateFav(e, added, productCode){
+        e.preventDefault();
+        if(this.state.fav_added){
+            removeFromFav(productCode);
+            this.setState({
+                fav_class : "add-to-fav",
+            });
+        }
+        else{
+            addToFav(productCode);
+            this.setState({
+                fav_class : "add-to-fav added-to-fav",
+            });
+        }
+    };
+    
+    render(){
+        let imgStyle = {
+            background: '#c6c6c6'
+        };
+
+        if(this.props.product.image != null){
+            imgStyle = {
+                background: `url(${this.props.product.image}) no-repeat center center`,
+                backgroundColor: '#c6c6c6',
+                backgroundSize: 'cover'
+            }
+        }
+
+        return (
+            <div id={ this.props.product.id }className="product-box">
+                <Link className="product-box-link" to={ `/product/${this.props.product.id}` }>
+                    <div className="product-img-box" style={ imgStyle }>
+                        <div className="product-name">{ this.props.product.name }</div>
+                        <div className="product-desc">
+                            <div className="p-name">
+                                { this.props.product.name }
+                            </div>
+                            <div className="p-desc">
+                                { this.props.product.desc }
+                            </div>
                         </div>
                     </div>
-                </div>
-            </Link>   
-            <div className="product-box-meta">
-                from <span className="p-price">${ props.product.price }</span> 
-                <a href="#" className="add-to-fav" onClick={ () => addToFav(props.product.id) } ></a>
-            </div>   
-        </div>
-    );
+                </Link>   
+                <div className="product-box-meta">
+                    from <span className="p-price">${ this.props.product.price }</span> 
+                    <a href="#" className={ this.state.fav_class } onClick={ (e) => this.updateFav(e, this.state.fav_added, this.props.product.id) } ></a>
+                </div>   
+            </div>
+        );
+    }
 }
--- client/src/js/helper.js
+++ client/src/js/helper.js
@@ -2,8 +2,31 @@ import Cookies from 'universal-cookie';
 
 export const addToFav = (productCode) => {
     const cookies = new Cookies();
-    cookies.set("favorites", productCode, { path: '/'});
-    // console.log(cookies.get('favorites'));
-    console.log("---------------");
-    console.log(cookies.getAll());
+    var fav = [];
+    if(cookies.get("favorites")){
+        fav = cookies.get("favorites");
+    }
+    fav.push(productCode);
+    cookies.set("favorites", fav, { path: '/'});
+    console.log(cookies.get("favorites"));
 };
+
+export const removeFromFav = (productCode) => {
+    const cookies = new Cookies();
+    var fav = cookies.get("favorites");
+    fav = fav.filter( item => item !== productCode );
+    cookies.set("favorites", fav, { path: '/'});
+    console.log(cookies.get("favorites"));
+}
+
+export const checkFavExits = (productCode) => {
+    const cookies = new Cookies();
+    let fav = cookies.get("favorites");
+    let ex = false;
+    fav = fav.filter( (item) => {
+        if(item == productCode){
+            ex = true;
+        }  
+    });
+    return ex;
+}
