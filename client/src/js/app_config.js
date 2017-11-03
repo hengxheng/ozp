cacc2ca5b90bda192e59af228430b6d741c33df2
Heng Zou <hzou@GSQUARED.local>
2017-11-03 15:09:45 +1100

    remove ac

--- client/src/js/app_config.js
+++ client/src/js/app_config.js
@@ -1,8 +1,5 @@
 const app_config = {
-    api_url: "/",
-    instra_cid: "3c03f384e2a94afb93bccf527cfbf847",
-    instra_code: "ec45984809544deda3194957baf29fb2",
-    ins_access_token: "2262223300.1677ed0.26f8fa2f5580471588da89d3d5aa9b96"
+    api_url: "/"
 };
 
 export default app_config;
