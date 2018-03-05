1172a3b64334d09ba09a9481dde106ee4f7a5577
hengxheng <hengxheng@hotmail.com>
2018-03-06 09:16:45 +1100

    case sensitive

---
+++ routes/Category.js
@@ -0,0 +1,51 @@
+const axios = require("axios"); // fetch json data, support promises and parallel request
+const config = require("../config");
+import Category from '../modules/category';
+
+
+// test category id: 29962
+
+    export const getCategory = (res, categoryId="") => {
+        let url = `https://api.rezdy.com/v1/categories/${categoryId}?apiKey=${config.Api_key}`;
+        axios.get(url, {
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
+                'Accept': 'application/json'
+            }
+        })
+        .then(response => {
+            if(response.data.requestStatus.success){
+                res.setHeader('Content-Type', 'application/json'); 
+                res.json(response.data.category);
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+    
+    export const getCategories = (req, res) => {
+    
+        let url = `https://api.rezdy.com/v1/categories?apiKey=${config.Api_key}`;
+    
+        axios.get(url, {
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
+                'Accept': 'application/json'
+            }
+        })
+        .then(response => {
+            if(response.data.requestStatus.success){
+                res.setHeader('Content-Type', 'application/json');
+                res.json(response.data.categories);
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+
+
+
+
+// module.exports = { getCategory, getCategories };
---
+++ routes/Product.js
@@ -0,0 +1,88 @@
+const axios = require("axios"); // fetch json data, support promises and parallel request
+const config = require("../config");
+
+// test product code: PZELUV
+// test category id: 29962
+export const getProduct = (res, productCode="") => {
+    let url = `https://api.rezdy.com/v1/products/${productCode}?apiKey=${config.Api_key}`;
+
+
+    axios.get(url, {
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Accept': 'application/json'
+        }
+    })
+    .then(response => {
+        if(response.data.requestStatus.success){
+            res.setHeader('Content-Type', 'application/json');
+            res.json(response.data.product);
+        }
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
+
+export const getProducts = (res, productCode="") => {
+    
+    let url = `https://api.rezdy.com/v1/products?apiKey=${config.Api_key}`;
+    axios.get(url, {
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Accept': 'application/json'
+        }
+    })
+    .then(response => {
+        if(response.data.requestStatus.success){
+            res.setHeader('Content-Type', 'application/json');
+            res.json(response.data.products);
+        }
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
+
+export const getProductByCategory = (res, categoryId) => {
+    let url = "";
+    if(categoryId === ""){
+        url = `https://api.rezdy.com/v1/products?apiKey=${config.Api_key}`;
+    }
+    else{
+        url = `https://api.rezdy.com/v1/categories/${categoryId}/products?apiKey=${config.Api_key}`;
+    }
+
+    axios.get(url, {
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Accept': 'application/json'
+        }
+    })
+    .then(response => {
+        res.setHeader('Content-Type', 'application/json');
+        if(response.data.requestStatus.success){
+            res.json(response.data.products);
+        }
+    })
+    .catch(error => {
+        console.log(error);
+    });
+};
+
+export const getProductFromDrupal = (res, productCode="") => {
+    const url = `${config.drupal_url}package/${productCode}`;
+    axios.get(url, {
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Accept': 'application/json'
+        }
+    })
+    .then(response => {
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response.data[0]);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
